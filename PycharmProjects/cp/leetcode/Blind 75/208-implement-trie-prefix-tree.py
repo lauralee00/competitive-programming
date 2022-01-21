48273bcba18f297ba65e8a1183773e182b976247
incompetentcoderoops <laura.yeheun.lee@gmail.com>
2022-01-21 14:06:53 -0500

    Blind 75

---
+++ PycharmProjects/cp/leetcode/Blind 75/208-implement-trie-prefix-tree.py
@@ -0,0 +1,57 @@
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+read = lambda: map(int, input().split())
+
+# Time Complexity:
+# insert -> O(N)
+# search -> O(N)
+# startsWith -> O(N)
+
+class TrieNode:
+    def __init__(self, char=""):
+        self.char = char
+        self.children = {}
+        self.end = False
+
+
+class Trie:
+
+    def __init__(self, char=""):
+        self.root = TrieNode()
+        # this is the head, no char stored, only children that serve as the first letters of all the words
+
+    def insert(self, word: str) -> None:
+        node = self.root  # create a reference
+        for ch in word:
+            if ch in node.children:
+                node = node.children[ch]
+            else:
+                new_node = TrieNode(ch)
+                node.children[ch] = new_node
+                node = new_node
+        node.end = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                return False
+            else:
+                node = node.children[ch]
+        return node.end
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+            else:
+                node = node.children[ch]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
--- PycharmProjects/cp/leetcode/Blind 75/79-word-search.py
+++ PycharmProjects/cp/leetcode/Blind 75/79-word-search.py
@@ -3,36 +3,38 @@ import sys
 input = lambda: sys.stdin.readline().strip()
 read = lambda: map(int, input().split())
 
+# Time Complexity : O ( m * n * 3^k ) -> k = len(word)
+# Space Complexity: O (     1       ) -> board doesn't count
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def valid(row, col):
             return 0 <= row < m and 0 <= col < n
 
-        def check(row, col, index, visited):
+        def check(row, col, index):
 
             if index == len(word):
                 return True
 
             if not valid(row, col) or \
                     word[index] != board[row][col] or \
-                    visited[row][col]:
+                    not board[row][col]:
                 return False
-            visited[row][col] = 1
 
-            res = check(row - 1, col, index + 1, visited) or \
-                  check(row + 1, col, index + 1, visited) or \
-                  check(row, col - 1, index + 1, visited) or \
-                  check(row, col + 1, index + 1, visited)
+            # mark visited on board, save board value as char
+            char, board[row][col] = board[row][col], None
 
-            visited[row][col] = 0
+            res = check(row - 1, col, index + 1) or \
+                  check(row + 1, col, index + 1) or \
+                  check(row, col - 1, index + 1) or \
+                  check(row, col + 1, index + 1)
+
+            board[row][col] = char # backtracking
             return res
 
         m, n = len(board), len(board[0])
-        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        visited = [[0] * n for _ in range(m)]
 
         for i in range(m):
             for j in range(n):
-                if check(i, j, 0, visited): return True
+                if check(i, j, 0): return True
         return False
